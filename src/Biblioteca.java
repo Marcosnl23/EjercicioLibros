daaefd3967fe98ac61a7519e14a418cbb2903111
Marcosnl23 <navidadleonmarcos@gmail.com>
2024-10-02 17:16:12 +0200

    Finalizado

---
+++ src/Biblioteca.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Biblioteca {
+    ArrayList<Libro> libros = new ArrayList<Libro>();
+
+    public void agregarLibros(ArrayList<Libro> nuevosLibros) {
+        libros.addAll(nuevosLibros);
+    }
+
+    public void mostrarlibros(ArrayList<Libro> libros){
+        for(Libro libro :libros) {
+            System.out.println(libro.toString());
+        }
+    }
+
+    public void seriailazrlibros(ArrayList<Libro> libros){
+        try (FileOutputStream fileOutput = new FileOutputStream("biblioteca.ser");
+             ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutput)) {
+
+            objectOutput.writeObject(libros);
+
+            System.out.println("Los libros han sido almacenados en el archivo libros.dat.");
+
+        } catch (IOException e) {
+            System.err.println("Error al almacenar los libros: " + e.getMessage());
+        }
+    }
+
+    public void deserializarlibros(){
+        try (FileInputStream fileInput = new FileInputStream("biblioteca.ser");
+             ObjectInputStream objectInput = new ObjectInputStream(fileInput)) {
+
+            while (true) {
+                try {
+                    // Leer y deserializar la lista completa de libros
+                    ArrayList<Libro> libros = (ArrayList<Libro>) objectInput.readObject();
+                    System.out.println("\nInformación del libro:");
+                    mostrarlibros(libros);
+                }catch (EOFException e) {
+                    System.err.println("Alcanzado el final del archivo");
+                    break; // Se alcanzó el final del archivo
+                }
+            }
+
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Error al leer el archivo libros.dat: " + e.getMessage());
+        }
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Libro libro1=new Libro("Cien años de soledad", "Gabriel García Márquez", "978-3-16-148410-0", 39.90, 5.00);
+        Libro libro2= new Libro("1984", "George Orwell", "978-0-452-28423-4", 29.99, 3.00);
+        Libro libro3=new Libro("El Principito", "Antoine de Saint-Exupéry", "978-3-16-148410-1", 19.95, 2.00);
+
+        ArrayList<Libro> libros= new ArrayList<Libro>();
+        libros.add(libro1);
+        libros.add(libro2);
+        libros.add(libro3);
+
+        Biblioteca biblioteca=new Biblioteca();
+        biblioteca.agregarLibros(libros);
+
+        biblioteca.seriailazrlibros(libros);
+
+        biblioteca.deserializarlibros();
+    }
+}
